f48cd60bb45e862ba6914a28891352d93dc482d8
Calebe Cabral <calebe.cabral16@outlook.com>
2021-02-24 14:53:52 -0300

    Import Countdown Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 
+import styles from '../styles/pages/Home.module.css';
+
 import { ExpBar } from '../components/ExpBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
-
-import styles from '../styles/pages/Home.module.css';
+import { Countdown } from '../components/Countdown';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
