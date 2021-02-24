b79899777ea0f996f0fb98c9d2b85d9f45c19159
Calebe Cabral <calebe.cabral16@outlook.com>
2021-02-24 14:46:50 -0300

    Add Profile and Completed Challenges Components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/CalebeCabral.png" alt=" Calebe Cabral" />
+
+      <div>
+        <strong>Calebe Cabral</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
