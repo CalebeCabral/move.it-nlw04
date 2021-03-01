fc278e054d19a42c4bd960affcaa4fb5f78916c2
Calebe Cabral <calebe.cabral16@outlook.com>
2021-03-01 20:03:17 -0300

    Add activeClassName to Navbar links; Add move.it logo to Navabr.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,27 @@ import Link from "./ActiveLink";
 
 import { HomeIcon } from "../icons/HomeIcon";
 import { AwardIcon } from "../icons/AwardIcon";
+
 import styles from "../styles/components/Navbar.module.css";
 
 export function Navbar() {
   return (
     <nav className={styles.container}>
-      <Link href="/" activeClassName={styles.selected}>
-        <a>
-          <HomeIcon />
-        </a>
-      </Link>
-      <Link href="/leaderboard" activeClassName={styles.selected}>
-        <a>
-          <AwardIcon />
-        </a>
-      </Link>
+      <div>
+        <img src="./logo.svg" alt="Move.it" />
+      </div>
+      <div className={styles.links}>
+        <Link href="/" activeClassName={styles.selected}>
+          <a>
+            <HomeIcon />
+          </a>
+        </Link>
+        <Link href="/leaderboard" activeClassName={styles.selected}>
+          <a>
+            <AwardIcon />
+          </a>
+        </Link>
+      </div>
     </nav>
   );
 }
